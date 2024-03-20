0e9836d039636fb4bacc57ada164d6b8cdf2eccc
Sushant920 <sushantchavan920@gmail.com>
2024-03-20 16:12:06 +0530

    Initial Commit

---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Sushant Chavan on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
